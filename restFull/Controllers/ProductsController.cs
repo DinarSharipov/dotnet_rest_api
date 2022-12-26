2fe908610e46966836c1dbd29fcfe09786d60e6d
Dinar Sharipov <Donotcoverr@mail.ru>
2022-12-26 20:35:36 +0300

    test_1

--- restFull/Controllers/ProductsController.cs
+++ restFull/Controllers/ProductsController.cs
@@ -7,12 +7,12 @@ namespace restFull.Controllers
 	[Route("/api/[controller]")]
 	public class ProductsController : Controller
 	{
-		private static List<Product> products = new List<Product>(new[]
+		private static List<Product> products = new List<Product>()
 		{
 			new Product() {Id = 1, Name = "Yahooo", Price = 12000},
 			new Product() {Id = 2, Name = "Google", Price = 96000},
 			new Product() {Id = 3, Name = "Apple", Price = 120000},
-		});
+		};
 
 		[HttpGet]
 		public IEnumerable<Product> Get() => products;
